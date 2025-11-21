e762ffefda3a8f0d5a5a7611e034134760e7fb8b
Manny <mannybeneda@gmail.com>
2025-11-21 03:28:34 -0700

    Create Simple_SQL.sql
    Add initial groceries table script

---
+++ Simple_SQL.sql
@@ -0,0 +1,18 @@
+CREATE TABLE groceries (
+       id INTEGER,
+       product TEXT,
+       quantity INTEGER,
+       price REAL
+);
+
+INSERT INTO groceries (id, product, quantity, price)
+VALUES (1, 'Carrots', 4, 3.99);
+
+INSERT INTO groceries (id, product, quantity, price)
+VALUES (2, 'Sour Dough Bread', 2, 8.99);
+
+INSERT INTO groceries (id, product, quantity, price)
+VALUES (3, 'Pistacio Pudding case x6', 2, 14.99);
+
+INSERT INTO groceries (id, product, quantity, price)
+VALUES (4, 'Milk jug', 2, 11.98);
